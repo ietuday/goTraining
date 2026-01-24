097846126ded842709faa11fb263feeef1960f31
Udayaditya Singh <udayaditya.singh@gmail.com>
2026-01-24 12:12:22 +0530

    Implmented osrm API

---
+++ microservices-go-starter/services/api-gateway/http.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"ride-sharing/shared/contracts"
+)
+
+func handleTripPreview(w http.ResponseWriter, r *http.Request) {
+	var reqBody previewTripRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	//Validation
+	if reqBody.UserID == "" {
+		http.Error(w, "user ID is required", http.StatusBadRequest)
+		return
+	}
+
+	jsonBody, _ := json.Marshal(reqBody)
+	reader := bytes.NewReader(jsonBody)
+
+	// Make the HTTP POST request
+	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var respBody any
+	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+		http.Error(w, "failed to parse response JSON data", http.StatusBadRequest)
+		return
+	}
+
+	response := contracts.APIResponse{Data: respBody}
+
+	writeJSON(w, http.StatusCreated, response)
+}
