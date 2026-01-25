a71ab165b9da398d3951cba7e89068b87d238a60
Udayaditya Singh <udayaditya.singh@gmail.com>
2026-01-25 16:34:01 +0530

    grpc trip proto added

--- microservices-go-starter/services/api-gateway/http.go
+++ microservices-go-starter/services/api-gateway/http.go
@@ -5,12 +5,12 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"ride-sharing/services/api-gateway/grpc_clients"
 	"ride-sharing/shared/contracts"
 )
 
 func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 	var reqBody previewTripRequest
-
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
 		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
 		return
@@ -18,7 +18,7 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 
-	//Validation
+	// validation
 	if reqBody.UserID == "" {
 		http.Error(w, "user ID is required", http.StatusBadRequest)
 		return
@@ -27,7 +27,18 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 	jsonBody, _ := json.Marshal(reqBody)
 	reader := bytes.NewReader(jsonBody)
 
-	// Make the HTTP POST request
+	// Why we need to create a new client for each connection:
+	// because if a service is down, we don't want to block the whole application
+	// so we create a new client for each connection
+	tripService, err := grpc_clients.NewTripServiceClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Don't forget to close the client to avoid resource leaks!
+	defer tripService.Close()
+
+	// TODO: Call trip service
 	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
 	if err != nil {
 		log.Print(err)
@@ -38,7 +49,7 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 
 	var respBody any
 	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
-		http.Error(w, "failed to parse response JSON data", http.StatusBadRequest)
+		http.Error(w, "failed to parse JSON data from trip service", http.StatusBadRequest)
 		return
 	}
 
